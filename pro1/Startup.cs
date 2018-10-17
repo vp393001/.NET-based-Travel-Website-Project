407c9615c937ff3ea996e89dcc19526cfa788d49
hardik parmar <parmarhardik633@gmail.com>
2018-10-17 22:41:24 +0530

    Add project files.

---
+++ pro1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pro1.Startup))]
+namespace pro1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
